db8b1a897256b209d1366f7edf44b9ce1f95babd
Lilian Shih <l.shih@outlook.com>
2017-12-06 23:47:33 +0000

    added a index page for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,27 @@
 class Admin::CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+    
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
