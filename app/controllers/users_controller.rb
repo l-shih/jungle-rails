1cddd84f420b2b3206be58e5d149e0007ac9b2b0
Lilian Shih <l.shih@outlook.com>
2017-12-07 20:50:46 +0000

    password and password confirmation will now work, and implemented user sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,26 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to [:root], notice: 'New user registered!'
+    else
+      render 'new'
+    end
+
+  end
+
+  def index
+    #redirect_to [:root]
+    @current_user = User.find(session[:user_id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
